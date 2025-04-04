e5a6f8ff5a707fdc640d2f3746dd2bfb3135d112
Vessel <phmacielv@gmail.com>
2025-04-04 10:38:05 -0300

    Add files via upload

---
+++ assets/javascript/script.js
@@ -0,0 +1,44 @@
+window.addEventListener("load", function () {
+    document.querySelector(".personal-photo img").classList.add("show");
+});
+
+
+
+
+function escreverTexto(elementoID, texto, velocidade) {
+    let elemento = document.getElementById(elementoID);
+    let index = 0;
+
+    
+    elemento.innerHTML = "";
+
+    function escrever() {
+        if (index < texto.length) {
+            elemento.innerHTML += texto.charAt(index);
+            index++;
+            setTimeout(escrever, velocidade);
+        }
+    }
+    escrever();
+}
+
+window.addEventListener("load", function () {
+    escreverTexto("texto2", "Olá, bem-vindo ao meu Portifólio!", 75);
+    escreverTexto("texto1", "Meu nome é Pedro Henrique!", 75);
+});
+
+function verificarScroll() {
+    const elementos = document.querySelectorAll(".ferramentas-icone i.escondido");
+
+    elementos.forEach((el) => {
+        let posicao = el.getBoundingClientRect().top;
+        let alturaTela = window.innerHeight;
+
+        if (posicao < alturaTela - 200) {
+            el.classList.add("visivel");
+        }
+    });
+}
+
+window.addEventListener("scroll", verificarScroll);
+verificarScroll();
